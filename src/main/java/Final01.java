1cd4ed4d7d67269a6eb5175d56225df095d35574
Preedaphong Rattanaprach <preedaphong.r@kasikornbank.com>
2017-08-04 15:43:44 +0700

    add case more than one digit

--- src/main/java/Final01.java
+++ src/main/java/Final01.java
@@ -1,7 +1,36 @@
 public class Final01{
 
     public long findMax(long number){
+        if(number<10){
+            return number;
+        }
+        else{
+            for (long i = number; i > 1; i--) {
+                String num = String.valueOf(i);
+                boolean check = true;
+                if(checkEndNine(num)){
 
+                    for (int j = 0; j < num.length()-1; j++) {
+                        if(Integer.valueOf(num.charAt(j)) > Integer.valueOf(num.charAt(j+1))){
+                            check = false;
+                            break;
+                        }
+                    }
+                    if (check) {
+                        return i;
+                    }
+
+                }
+
+            }
+        }
         return number;
+    }
+
+    public boolean checkEndNine(String num){
+        if (num.charAt(num.length()-1) == '9') {
+            return true;
+        }
+        else return false;
     }
 }
--- src/test/java/Test01.java
+++ src/test/java/Test01.java
@@ -14,5 +14,52 @@ public class Test01{
             assertEquals(expectedResult, actualResult);
         }
 
+    @Test
+    public void twoDigit(){
+        long actualResult = final01.findMax(10);
+        long expectedResult = 9;
+        assertEquals(expectedResult, actualResult);
+    }
+    @Test
+    public void threeDigit(){
+        long actualResult = final01.findMax(132);
+        long expectedResult = 129;
+        assertEquals(expectedResult, actualResult);
+    }
+
+
+    @Test
+    public void threeDigit2(){
+        long actualResult = final01.findMax(111);
+        long expectedResult = 99;
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void fourDigit(){
+        long actualResult = final01.findMax(1200);
+        long expectedResult = 1199;
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void fourDigit2(){
+        long actualResult = final01.findMax(1186);
+        long expectedResult = 1179;
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void veryMuchDigit2(){
+        long actualResult = final01.findMax(1111110);
+        long expectedResult = 999999;
+        assertEquals(expectedResult, actualResult);
+    }
+   /* @Test
+    public void veryMuchDigit(){
+        long actualResult = final01.findMax(111111111111111110);
+        long expectedResult = 99999999999999999;
+        assertEquals(expectedResult, actualResult);
+    }*/
 
 }
